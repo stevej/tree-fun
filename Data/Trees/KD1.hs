8301df4dff70b2106dc065dd445ba4fa4704ba9d
Steve Jenson <stevej@fruitless.org>
2015-06-20 12:27:08 -0700

    making a slightly nicer interface

--- Data/Trees/KD1.hs
+++ Data/Trees/KD1.hs
@@ -5,15 +5,18 @@ import Data.Ord (comparing)
 
 type Point = [Int]
 
-data Node = Branch Point Node Node | Leaf Point | Empty deriving (Show)
+data KDTree = Branch Point KDTree KDTree | Leaf Point | Empty deriving (Show)
 
 -- | building a KD tree from a list of Points.
 --
--- >>> kd_from_list [[2,3], [5,4], [9,6], [4,7], [8,1], [7,2]] 0
+-- >>> kd_from_list [[2,3], [5,4], [9,6], [4,7], [8,1], [7,2]]
 -- Branch [7,2] (Branch [5,4] (Branch [2,3] Empty Empty) (Branch [4,7] Empty Empty)) (Branch [9,6] (Branch [8,1] Empty Empty) Empty)
-kd_from_list :: [Point] -> Int -> Node
-kd_from_list [] depth = Empty
-kd_from_list points depth = Branch median (kd_from_list beginning_of_list (depth + 1)) (kd_from_list end_of_list (depth + 1))
+kd_from_list :: [Point] -> KDTree
+kd_from_list points = kd_from_list' points 0
+
+kd_from_list' :: [Point] -> Int -> KDTree
+kd_from_list' [] depth = Empty
+kd_from_list' points depth = Branch median (kd_from_list' beginning_of_list (depth + 1)) (kd_from_list' end_of_list (depth + 1))
                             where
                               dimension = length $ head points -- guaranteed to not be empty due to pattern matching
                               sortFn = comparing (\xs -> xs !! (depth `mod` dimension))
@@ -24,5 +27,3 @@ kd_from_list points depth = Branch median (kd_from_list beginning_of_list (depth
                               end_of_list = tail xs
 
 
-main = do
-  putStrLn(show $ kd_from_list [[3,4],[1,2],[7,8]] 0)
